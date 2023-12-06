1c756856d0a4e7682c342ae444d46d6cc33dd653
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-12-06 03:04:14 +0300

    Initial REST for notes

--- Backend/controllers/notesController.js
+++ Backend/controllers/notesController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Note = require('../models/notesModel')
 
 //Get User note
 //GET Request /api/notes
 //Private
 const getNotes = asyncHandler (async(req, res) => {
-    res.status(200).json({ message: 'Get notes' })
+    const notes = await Note.find()
+    res.status(200).json(notes)
 })
 
 //Add note
@@ -15,21 +17,44 @@ const addNote = asyncHandler (async(req, res) => {
         res.status(400)
         throw new Error('Please add a text field to continue')
     }
-    res.status(200).json({ message: 'Create note' })
+
+    const note = await Note.create({
+        text: req.body.text,
+    })
+    res.status(200).json(note)
 })
 
 //Edit note
 //PUT Request /api/notes/:id
 //Private
 const editNote = asyncHandler (async(req, res) => {
-    res.status(200).json({ message: `Update note ${req.params.id}` })
+    const note = await Note.findById(req.params.id)
+
+    if(!note) {
+        res.status(400)
+        throw new Error('Note not found')
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(updatedNote)
 })
 
 //Delete note
 //DELETE Request /api/notes/:id
 //Private
 const deleteNote = asyncHandler (async(req, res) => {
-    res.status(200).json({ message: `Delete note ${req.params.id}` })
+    const note = await Note.findById(req.params.id)
+
+    if(!note) {
+        res.status(400)
+        throw new Error('Note not found')
+    }
+
+    await note.deleteOne()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 //Get User note
